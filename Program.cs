3081b51009db3e0fb9c2cb049b814c30baad640c
Theo-Parker <theoparker111@gmail.com>
2025-10-02 23:39:12 +1300

    implemented start of user input

--- Program.cs
+++ Program.cs
@@ -13,4 +13,39 @@ Console.WriteLine();
 Console.WriteLine($"{reportService.GetMonthlySummary()}");
 Console.WriteLine();
 Console.WriteLine($"{reportService.GetYearlySummary()}");
-Console.WriteLine();
+Console.WriteLine("\n\n\n\n");
+
+bool running = true;
+do
+{
+    Console.WriteLine("""
+    What would you like to do?
+    1. Make a transaction
+    2. See your balance
+    3. See your monthly transaction summary
+    4. See your yearly transaction summary
+    5. Exit
+    """);
+
+    string input = Console.ReadLine();
+    bool valid = int.TryParse(input, System.Globalization.CultureInfo.InvariantCulture, out int result);
+    if (!valid) { continue; }
+    switch (result)
+    {
+        case 1:
+            MakeTransaction();
+            break;
+        default:
+            break;
+    }
+} while (running);
+
+void MakeTransaction()
+{
+    bool makingTransaction = true;
+    do
+    {
+        Console.WriteLine("Make a transaction");
+        makingTransaction = false;
+    } while (makingTransaction);
+};
