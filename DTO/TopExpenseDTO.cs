80cec54a7a673785b60f29c1aa63e882e4dae18c
Theo-Parker <theoparker111@gmail.com>
2025-10-02 14:02:49 +1300

    rename top expense DTO

--- DTO/TopExpenseDTO.cs
+++ DTO/TopExpenseDTO.cs
@@ -2,7 +2,7 @@ namespace pfin.DTO;
 
 public class TopExpenseDTO
 {
-    public string Description { get; init; }
+    public string Description { get; init; } = "No Description";
     public decimal Amount { get; init; }
     public DateOnly date { get; init; }
 }
